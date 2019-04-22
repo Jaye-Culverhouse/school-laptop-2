233aa8492c1df4ff45295b091540d56b1f3fe1db
Jade <jadehculverhouse@gmail.com>
2019-04-23 00:18:47 +0100

    Started work on addingn mysql db

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import client_cv
 import settingobj
-
+import pymysql
 
 def main():
 
@@ -8,10 +8,28 @@ def main():
 
 	while True:
 		QRData = client_cv.readUntilQRFound()[0]
-		
+		print(QRData)
+		conn = pymysql.connect(
+			host = setting_obj.get("ip"), 
+			user = setting_obj.get("username"), 
+			password = setting_obj.get("password"),
+			db = setting_obj.get("db"),
+			charset = "utf8mb4",
+			cursorclass=pymysql.cursors.DictCursor)
 
+		# try:
 
+		with conn.cursor() as c:
 
+			sql = "SELECT CheckedIn FROM Device WHERE uid=%s"
+			c.execute(sql, (QRData["uid"]))
+
+			result = c.fetchone()
+
+			print(result)
+
+		# finally:
+			c.close()
 
 
 if __name__ == '__main__':
