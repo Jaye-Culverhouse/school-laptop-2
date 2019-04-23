d0cc19db615e592838e4352509131f73f76832ca
Jade <jadehculverhouse@gmail.com>
2019-04-23 01:00:42 +0100

    Added save function and set function

--- settingobj.py
+++ settingobj.py
@@ -3,11 +3,18 @@ import json
 class obj():
 
 	def __init__(self, fn):
+		self.fn = fn
 
 		with open(fn) as f:
-
 			self.settings = json.load(f)
-
 
 	def get(self, setting):
 		return self.settings[setting]
+
+	def set(self, setting, newval):
+		self.settings[setting] = newval
+		self.save()
+
+	def save(self):
+		with open(self.fn, 'w') as f:
+			json.dump(self.settings,f)
